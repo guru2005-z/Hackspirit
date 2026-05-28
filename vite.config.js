b17d3a746acdb573c1b46f298423aa0c8930b5dd
Guru <kattaguravaiah00@gmail.com>
2026-05-28 09:48:42 +0530

    Register form

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        register: resolve(__dirname, 'register.html'),
       },
     },
   },
